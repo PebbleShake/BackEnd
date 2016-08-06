93888ea1b5874fd9c2acf8ebcf2e81d30b32462f
emanueledivizio <emadivizio@gmail.com>
2016-08-06 17:28:27 +0200

    commit

--- index.js
+++ index.js
@@ -34,15 +34,39 @@ router.get('/handshake', function(req, res){
   res.json({message: "WELCOME!"})
 })
 
-router.post('/shake', function(req, res){
-  res.json({message: req.body.name + " shook hand!"})
-})
+var hands = [];
 
+router.post('/shake', function(req, res){
+  var hand = {
+    name: req.body.name
+  }
+  shake(hand, hands)
+})
 
 app.use("/api", router)
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+//CODE
+
+
+function shake(hand, hands){
+  if(hands.length < 2){
+    hands.push(hand)
+  }else{
+    return shakeHands(hands)
+  }
+  return hands
+}
+
+function shakeHands(hands){
+  var hand1 = hands[0]
+  var hand2 = hands[1]
+  console.log(hand1.name + " shook hands with " +  hand2.name)
+  return []
+}
+
 
 
